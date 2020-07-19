4e70a38107614a47255ab5a3e2f0ca24ded8c9c2
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-07-19 15:25:23 -0500

    Added style scale on img and background with overflow hidden

--- src/Components/MenuItem/MenuItem.js
+++ src/Components/MenuItem/MenuItem.js
@@ -3,14 +3,15 @@ import "./menu-item.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-      className={`menu-item ${size}`}
-    >
+    <div className={`menu-item ${size}`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">Shop Now</span>
       </div>
     </div>
