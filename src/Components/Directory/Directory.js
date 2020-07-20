375acf6ada051da4b5d51c598b6841b18113cbcf
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-07-19 21:36:20 -0500

    Added picture and two more categories to directory

--- src/Components/Directory/Directory.js
+++ src/Components/Directory/Directory.js
@@ -6,26 +6,30 @@ class Directory extends Component {
   state = {
     sections: [
       {
-        title: "hats",
-        imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+        title: "Jeans",
+        imageUrl:
+          "https://res.cloudinary.com/duprwuo4j/image/upload/v1595211674/h-charis-online-store/jason-leung-DmD8HVOjy4c-unsplash_vlhbxm.jpg",
         id: 1,
         linkUrl: "shop/hats",
       },
       {
-        title: "jackets",
-        imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+        title: "JACKETS & COATS",
+        imageUrl:
+          "https://res.cloudinary.com/duprwuo4j/image/upload/v1595212188/h-charis-online-store/marcos-rivas-OZokCdO5goI-unsplash_gwivix.jpg",
         id: 2,
         linkUrl: "shop/jackets",
       },
       {
         title: "sneakers",
-        imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+        imageUrl:
+          "https://res.cloudinary.com/duprwuo4j/image/upload/v1595211757/h-charis-online-store/jayson-hinrichsen-qLs4WYXqLNY-unsplash_nxhhk3.jpg",
         id: 3,
         linkUrl: "shop/sneakers",
       },
       {
         title: "womens",
-        imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+        imageUrl:
+          "https://res.cloudinary.com/duprwuo4j/image/upload/v1595211465/h-charis-online-store/liz_uoljkf.jpg",
         size: "large",
         id: 4,
         linkUrl: "shop/womens",
