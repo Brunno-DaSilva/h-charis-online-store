3ee0772c5223097619a7dc26d0d489ae42f3f0cc
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-07-21 22:33:58 -0500

    added shopbag icon

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/hcharis-01.svg/hcharis-01.svg";
 import SearchArea from "../SearchArea/SearchArea";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
+
 import "./header.scss";
 
 const Header = () => {
@@ -23,6 +26,9 @@ const Header = () => {
           </Link>
           <Link className="option" to="/contact">
             CONTACT
+          </Link>
+          <Link className="option" to="/contact">
+            <FontAwesomeIcon className="info-icons" icon={faShoppingBag} />
           </Link>
         </div>
       </div>
