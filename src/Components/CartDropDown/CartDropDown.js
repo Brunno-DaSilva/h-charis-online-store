664e6ac0ccf50c80abddc5b61b83fbbb64e2a5e4
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-08-04 12:27:38 -0500

    created conditional for empty cart and added style

--- src/Components/CartDropDown/CartDropDown.js
+++ src/Components/CartDropDown/CartDropDown.js
@@ -11,9 +11,13 @@ const CartDropDown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        {cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
       <CustomButton>Check Out Items</CustomButton>
     </div>
