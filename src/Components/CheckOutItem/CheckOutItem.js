afb5175fecf9c4789840a4a2eb7f37e27a948901
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-08-06 16:08:48 -0500

    added increase and decrease cart item func

--- src/Components/CheckOutItem/CheckOutItem.js
+++ src/Components/CheckOutItem/CheckOutItem.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
-import { clearItemFromCart } from "../../Redux/Cart/cart-action";
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from "../../Redux/Cart/cart-action";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 import "./checkout-Item.scss";
+import { addItemToCart } from "../../Redux/Cart/cart-utils";
 
-const CheckOutItem = ({ cartItem, clearItem }) => {
+const CheckOutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, price, imageUrl, quantity } = cartItem;
 
   return (
@@ -18,10 +23,17 @@ const CheckOutItem = ({ cartItem, clearItem }) => {
 
       <span className="name">{name}</span>
       <span className="price">$ {price}</span>
-      <span className="quantity">
-        <FontAwesomeIcon className="less" icon={faMinus} /> {quantity}{" "}
-        <FontAwesomeIcon className="plus" icon={faPlus} />
-      </span>
+      <div className="quantity">
+        <span onClick={() => removeItem(cartItem)}>
+          {" "}
+          <FontAwesomeIcon className="less" icon={faMinus} />{" "}
+        </span>
+        {quantity}
+        <span onClick={() => addItem(cartItem)}>
+          {" "}
+          <FontAwesomeIcon className="plus" icon={faPlus} />
+        </span>
+      </div>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
       </div>
@@ -31,6 +43,8 @@ const CheckOutItem = ({ cartItem, clearItem }) => {
 
 const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CheckOutItem);
