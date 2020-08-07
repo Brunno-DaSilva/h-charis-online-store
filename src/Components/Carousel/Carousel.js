d5d08ed8eba09fa60428d6db6b3a55a8a221c788
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-08-07 16:07:01 -0500

    added carousel imgs and adjusted settings, and its width

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -8,6 +8,7 @@ class Carousel extends Component {
   render() {
     const settings = {
       dots: true,
+      infinite: true,
       arrows: false,
       speed: 500,
       slidesToShow: 1,
@@ -18,13 +19,13 @@ class Carousel extends Component {
       <div className="carousel-container">
         <Slider {...settings}>
           <div>
-            <img src="http://placekitten.com/g/400/200" />
+            <img src="https://res.cloudinary.com/duprwuo4j/image/upload/v1596833062/h-charis-online-store/3_lmcig0.png " />
           </div>
           <div>
-            <img src="http://placekitten.com/g/400/200" />
+            <img src="https://res.cloudinary.com/duprwuo4j/image/upload/v1596833062/h-charis-online-store/2_bwq1uv.png" />
           </div>
           <div>
-            <img src="http://placekitten.com/g/400/200" />
+            <img src="https://res.cloudinary.com/duprwuo4j/image/upload/v1596833062/h-charis-online-store/1_aidbr3.png" />
           </div>
         </Slider>
       </div>
