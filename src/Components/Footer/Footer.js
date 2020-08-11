9f2244b4b8546521bb8d38039b4ab6789962ef63
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-08-10 23:13:26 -0500

    finished footer for now

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,26 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { ReactComponent as Logo } from "../../assets/hcharis-01.svg/hcharis-01.svg";
+import { ReactComponent as Logo } from "../../assets/hcharis-01.svg/hcharis-02.svg";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
+
 import {
   faFacebook,
   faPinterest,
   faTwitter,
   faLinkedin,
+  faGithub,
 } from "@fortawesome/free-brands-svg-icons";
 
 import "./footer.scss";
 
 const Footer = () => {
+  const getYear = () => {
+    return new Date().getFullYear();
+  };
+
   return (
     <div className="footer">
       <div className="footer-brands">
-        <div className="nike">Nike</div>
-        <div className="adidas">adidas</div>
-        <div className="polo">polo</div>
-        <div className="lacoste">lacoste</div>
+        <div className="brands-logos">
+          <img src="https://res.cloudinary.com/duprwuo4j/image/upload/v1597118601/h-charis-online-store/icons8-nike-500_m3e7ad.png" />
+        </div>
+        <div className="brands-logos">
+          <img src="https://res.cloudinary.com/duprwuo4j/image/upload/v1597118601/h-charis-online-store/icons8-adidas-trefoil-500_kzy5lk.png" />
+        </div>
+        <div className="brands-logos">
+          <img src="https://res.cloudinary.com/duprwuo4j/image/upload/v1597118601/h-charis-online-store/icons8-air-jordan-500_xspox8.png" />
+        </div>
+        <div className="brands-logos">
+          <img src="https://res.cloudinary.com/duprwuo4j/image/upload/v1597118601/h-charis-online-store/icons8-etsy-480_ui34k1.png" />
+        </div>
       </div>
       <div className="footer-info">
         <div className="info-columns">
@@ -44,6 +59,7 @@ const Footer = () => {
                 className="info-icons social"
                 icon={faFacebook}
               />
+
               <FontAwesomeIcon
                 className="info-icons social"
                 icon={faPinterest}
@@ -55,20 +71,43 @@ const Footer = () => {
               />
             </div>
           </div>
+
           <div className="column-two">
             <p className="info-title">Information</p>
-            <span>About Us</span>
-            <span>Checkout</span>
-            <span>Contact</span>
-            <span>Career</span>
+            <div className="info-links">
+              <Link className="info-link" to="/">
+                About Us
+              </Link>
+              <Link className="info-link" to="/">
+                Checkout
+              </Link>
+              <Link className="info-link" to="/">
+                Contact
+              </Link>
+              <Link className="info-link" to="/">
+                Career
+              </Link>
+            </div>
           </div>
+
           <div className="column-three">
             <p className="info-title">My Account</p>
-            <span>My Account</span>
-            <span>Contact</span>
-            <span>Shopping Cart</span>
-            <span>Shop</span>
+            <div className="info-links">
+              <Link className="info-link" to="/">
+                My Account
+              </Link>
+              <Link className="info-link" to="/">
+                Contact
+              </Link>
+              <Link className="info-link" to="/">
+                Shopping Cart
+              </Link>
+              <Link className="info-link" to="/">
+                Shop
+              </Link>
+            </div>
           </div>
+
           <div className="column-four">
             <p className="info-title">Join Our Newsletter Now</p>
             <span>
@@ -77,11 +116,11 @@ const Footer = () => {
             <div className="footer__search-container">
               <form>
                 <input
-                  className="search-box"
+                  className="newsletter"
                   type="text"
-                  label="search"
-                  placeholder="search"
-                  name="search"
+                  label="newsletter"
+                  placeholder="Enter Your Email"
+                  name="newsletter"
                 />
                 <button id="search-btn" type="submit">
                   SUBSCRIBE
@@ -90,9 +129,51 @@ const Footer = () => {
             </div>
           </div>
         </div>
+
         <div className="bottom-section">
+          <img
+            className="b-logo"
+            src="https://res.cloudinary.com/duprwuo4j/image/upload/v1588899368/Logo/blogo-gray_doyoy3.png"
+          />
+          <p className="bruno">
+            <span>{getYear()}</span> Bruno DaSilva
+            <span> Front-End Developer</span>
+          </p>
+
+          <div className="more-projects">
+            <p>
+              More <span>Projects</span>
+            </p>
+
+            <div>
+              <a href="https://github.com/Brunno-DaSilva" target="_blank">
+                <i>
+                  <FontAwesomeIcon icon={faGithub} />
+                </i>
+              </a>
+
+              <a
+                href="https://www.linkedin.com/in/bruno-dasilva/"
+                target="_blank"
+              >
+                <i>
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </i>
+              </a>
+
+              <a href="http://bruno-dasilva.com/" target="_blank">
+                <i>
+                  <FontAwesomeIcon icon={faGlobeAmericas} />
+                </i>
+              </a>
+            </div>
+          </div>
+
           <div className="copyright">
-            <p>Copyright ©2020 All rights reserved - Bruno DaSilva</p>
+            <p>
+              Copyright <span>©{getYear()}</span> All rights reserved - Bruno
+              DaSilva Software Development
+            </p>
           </div>
         </div>
       </div>
