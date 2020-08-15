334a8e1baa4753e777a7a46f1c693d78bfe54003
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-08-14 20:38:54 -0500

    Created collectionOverview component, move map from shoppage

--- src/Pages/ShopPage/ShopPage.js
+++ src/Pages/ShopPage/ShopPage.js
@@ -1,27 +1,14 @@
 import React from "react";
-import { connect } from "react-redux";
-
-import { createStructuredSelector } from "reselect";
-import { selectShopCollections } from "../../Redux/Shop/shop-selector";
-
-import CollectionPreview from "../../Components/CollectionPreview/CollectionPreview";
+import CollectionOverview from "../../Components/CollectionOverview/CollectionOverview";
 
 import "./shop-page.scss";
 
-// import SHOP_DATA from "./ShopData";
-
-const ShopPage = ({ collections }) => {
+const ShopPage = () => {
   return (
     <div className="shop-page">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionOverview />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
